f990bfa38994c55cca96435adecb83e811b14e66
LiusirNick <gn02791401@gmail.com>
2023-01-30 00:36:19 +0800

    add hbs

--- app.js
+++ app.js
@@ -2,6 +2,10 @@ const express = require("express");
 const app = express();
 
 const mongoose = require("mongoose"); // 載入 mongoose
+const exphbs = require("express-handlebars");
+
+app.engine("hbs", exphbs({ defaultLayout: "main", extname: ".hbs" }));
+app.set("view engine", "hbs");
 
 if (process.env.NODE_ENV !== "production") {
   require("dotenv").config();
@@ -24,9 +28,8 @@ db.once("open", () => {
 });
 // 設定首頁路由
 app.get("/", (req, res) => {
-  res.send("hello world");
+  res.render("index");
 });
-
 // 設定 port 3000
 app.listen(3000, () => {
   console.log("App is running on http://localhost:3000");
