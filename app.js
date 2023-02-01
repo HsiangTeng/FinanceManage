e0947cf07871bbde1a6094cd48f12bd26fb8b4cd
LiusirNick <gn02791401@gmail.com>
2023-02-01 22:48:34 +0800

    feat: modify index.hbs

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const app = express();
 
 const mongoose = require("mongoose"); // 載入 mongoose
 const exphbs = require("express-handlebars");
+const Todo = require("./models/todo"); // 載入 Todo model
 
 app.engine("hbs", exphbs({ defaultLayout: "main", extname: ".hbs" }));
 app.set("view engine", "hbs");
@@ -26,11 +27,15 @@ db.on("error", () => {
 db.once("open", () => {
   console.log("mongodb connected!");
 });
-// 設定首頁路由
-app.get("/", (req, res) => {
-  res.render("index");
-});
+
 // 設定 port 3000
+app.get("/", (req, res) => {
+  Todo.find() // 取出 Todo model 裡的所有資料
+    .lean() // 把 Mongoose 的 Model 物件轉換成乾淨的 JavaScript 資料陣列
+    .then((todos) => res.render("index", { todos })) // 將資料傳給 index 樣板
+    .catch((error) => console.error(error)); // 錯誤處理
+});
+
 app.listen(3000, () => {
   console.log("App is running on http://localhost:3000");
 });
